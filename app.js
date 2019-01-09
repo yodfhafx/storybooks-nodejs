ae3f562cc016e6b113e0010252a65f63ff0ca824
yodfhafx <yodfha2014@gmail.com>
2019-01-09 18:50:16 +0700

    setup project

---
+++ app.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const mongoose = require('mongoose');
+
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send('It work');
+});
+
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => {
+  console.log(`Server run on port ${port}`);
+});
