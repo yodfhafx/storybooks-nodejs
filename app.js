276acf2b362b03471773c78c3a45f9f0be1aff0e
yodfhafx <yodfha2014@gmail.com>
2019-01-11 02:21:20 +0700

    create google strategy

--- app.js
+++ app.js
@@ -1,11 +1,21 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const passport = require('passport');
+
+// Passport Config
+require('./config/passport')(passport)
+
+// Load Routes
+const auth = require('./routes/auth');
 
 const app = express();
 
 app.get('/', (req, res) => {
   res.send('It work');
 });
+
+// Use Routes
+app.use('/auth', auth);
 
 const port = process.env.PORT || 3000;
 
