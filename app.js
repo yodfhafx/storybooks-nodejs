265b6df1ea174fafc3994625408b56a64bc56fcb
yodfhafx <yodfha2014@gmail.com>
2019-01-11 22:27:15 +0700

    saving authentication user

--- app.js
+++ app.js
@@ -1,17 +1,54 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const cookieParser = require('cookie-parser');
+const session = require('express-session');
 const passport = require('passport');
 
+// Load User Model
+require('./models/User');
+
 // Passport Config
-require('./config/passport')(passport)
+require('./config/passport')(passport);
 
 // Load Routes
 const auth = require('./routes/auth');
+
+// Load Keys
+const keys = require('./config/keys');
+
+// Map global promises
+mongoose.Promise = global.Promise;
+
+// Mongoose Connect
+mongoose.connect(keys.mongoURI, {
+  useNewUrlParser: true
+})
+.then(() => {
+  console.log('MongoDB Connected');
+})
+.catch(err => console.log(err));
 
 const app = express();
 
 app.get('/', (req, res) => {
   res.send('It work');
+});
+
+app.use(cookieParser());
+app.use(session({
+  secret: 'secret',
+  resave: false,
+  saveUninitialized: false
+}));
+
+// Passport Middleware
+app.use(passport.initialize());
+app.use(passport.session()); 
+
+// Set globall variables
+app.use((req, res, next) => {
+  res.locals.user = req.user || null;
+  next();
 });
 
 // Use Routes
--- config/keys.js
+++ config/keys.js
@@ -1,5 +1,5 @@
 module.exports = {
   mongoURI: 'mongodb://adminfha:93639363stack@ds253094.mlab.com:53094/storybooks-dev',
   googleClientID: '8886469331-qj2ui2q1nnbh1nut4ki14lo80cjvms9o.apps.googleusercontent.com',
-  goolgeClientSecret: 'I-UjeXjUPT27_8nOCALwSYSn'
+  googleClientSecret: 'I-UjeXjUPT27_8nOCALwSYSn'
 }
