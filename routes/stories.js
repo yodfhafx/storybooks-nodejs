9f5fed2cbb0f352195f276cdf08806efb340cec5
yodfhafx <yodfha2014@gmail.com>
2019-01-16 21:31:43 +0700

    fetch public stories

--- routes/stories.js
+++ routes/stories.js
@@ -7,7 +7,13 @@ const {ensureAuthenticated, ensureGuest} = require('../helpers/auth');
 
 // Stories Index
 router.get('/', (req, res) => {
-  res.render('stories/index');
+  Story.find({status: 'public'})
+    .populate('user')
+    .then(stories => {
+      res.render('stories/index', {
+        stories: stories
+      });
+  });
 });
 
 // Add Story Form
