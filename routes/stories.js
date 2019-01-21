0b4089b25cdc894b5da6d2cc3e9e445782b648cd
yodfhafx <yodfha2014@gmail.com>
2019-01-21 12:37:18 +0700

    implement add comments function

--- routes/stories.js
+++ routes/stories.js
@@ -113,5 +113,25 @@ router.delete('/:id', (req, res) => {
     });
 })
 
+// Add Comment
+router.post('/comment/:id', (req, res) => {
+  Story.findOne({
+    _id: req.params.id
+  })
+  .then(story => {
+    const newComment = {
+      commentBody: req.body.commentBody,
+      commentUser: req.user.id
+    }
+
+    // Push to comments array
+    story.comments.unshift(newComment);
+
+    story.save()
+      .then(story => {
+        res.redirect(`/stories/show/${story.id}`)
+      })
+  });
+});
 
 module.exports = router;
