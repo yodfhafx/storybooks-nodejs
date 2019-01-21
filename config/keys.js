16352761c287f791771b3d195352f176eabec9d6
yodfhafx <yodfha2014@gmail.com>
2019-01-21 16:12:02 +0700

    finish version 1.0

--- config/keys.js
+++ config/keys.js
@@ -1,5 +1,5 @@
-module.exports = {
-  mongoURI: 'mongodb://adminfha:93639363stack@ds253094.mlab.com:53094/storybooks-dev',
-  googleClientID: '8886469331-qj2ui2q1nnbh1nut4ki14lo80cjvms9o.apps.googleusercontent.com',
-  googleClientSecret: 'I-UjeXjUPT27_8nOCALwSYSn'
+if(process.envNODE_ENV === 'production'){
+  module.exports = require('./keys_prod');
+} else {
+  module.exports = require('./keys_dev');
 }
--- routes/stories.js
+++ routes/stories.js
@@ -9,6 +9,7 @@ const {ensureAuthenticated, ensureGuest} = require('../helpers/auth');
 router.get('/', (req, res) => {
   Story.find({status: 'public'})
     .populate('user')
+    .sort({date: 'desc'})
     .then(stories => {
       res.render('stories/index', {
         stories: stories
@@ -24,10 +25,46 @@ router.get('/show/:id', (req, res) => {
   .populate('user')
   .populate('comments.commentUser')
   .then(story => {
-    res.render('stories/show', {
-      story: story
-    });
+    if(story.status == 'public'){
+      res.render('stories/show', {
+        story: story
+      });
+    } else {
+      if(req.user){
+        if(req.user.id == story.user._id){
+          res.render('stories/show', {
+            story: story
+          });
+        } else {
+          res.redirect('/stories');
+        }
+      } else {
+        res.redirect('/stories');
+      }
+    }
   });
+});
+
+// List Stories from a user
+router.get('/user/:userId', (req, res) => {
+  Story.find({user: req.params.userId, status: 'public'})
+    .populate('user')
+    .then(stories => {
+      res.render('stories/index', {
+        stories: stories
+      });
+    });
+});
+
+// Logged in users stories
+router.get('/my', ensureAuthenticated, (req, res) => {
+  Story.find({user: req.body.id})
+    .populate('user')
+    .then(stories => {
+      res.render('stories/index', {
+        stories: stories
+      });
+    });
 });
 
 // Add Story Form
@@ -67,9 +104,13 @@ router.get('/edit/:id', ensureAuthenticated, (req, res) => {
     _id: req.params.id
   })
   .then(story => {
-    res.render('stories/edit', {
-      story: story
-    });
+    if(story.user != req.user.id){
+      res.redirect('/stories');
+    } else {
+      res.render('stories/edit', {
+        story: story
+      });
+    }
   });
 });
 
